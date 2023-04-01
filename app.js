4fcb17fc3f7b99c8ea36c848a66ba165b8433518
SonaliN84 <sonali.nilekar@gmail.com>
2023-04-01 16:32:18 +0530

    Sending response

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const http=require('http');
 
 const server=http.createServer((req,res)=>{
     console.log("sonali")
+    console.log(req.url,req.method)
+    // process.exit();
+    res.setHeader('Content-Type','text/html')
+    if(req.url==='/'){
+    res.write("<html><body><h1>Hello</h1></body></html>")
+    res.end();
+    return ;
+    }
+    else if(req.url==='/home'){
+        res.write("<html><body><h1>HOME</h1></body></html>")  
+        return res.end(); 
+    }
+    else if(req.url==='/about')
+    {
+        res.write("<html><body><h1>ABOUT</h1></body></html>") 
+        return res.end();  
+    }
+    res.write("<html><body><h1>hey,welcome</h1></body></html>")
+    res.end();
+   
+   
 })
 
-server.listen(4000);
+server.listen(3000);
