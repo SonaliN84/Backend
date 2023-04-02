2c138bf48b8cd574b6eec110c82d3414bed02753
SonaliN84 <sonali.nilekar@gmail.com>
2023-04-02 11:29:53 +0530

    Reading from file

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const http=require('http');
-
+const fs=require('fs')
 const server=http.createServer((req,res)=>{
     console.log("sonali")
     console.log(req.url,req.method)
     // process.exit();
     res.setHeader('Content-Type','text/html')
     if(req.url==='/'){
-    res.write("<html><body><h1>Hello</h1></body></html>")
-    res.end();
-    return ;
+    fs.readFile('message.txt',{encoding:"utf-8"},(err,data)=>{
+    if(err){
+        console.log(err)
+    }
+    res.write(`<html><body>${data}<form action='/message' method='POST'><input type='text' name='name'/><button type='submit'>send</button></form></body></html>`)
+    return res.end();
+
+   })
+   
+    }
+    else if(req.url==='/message' && req.method==='POST')
+    {    
+        const body=[];
+        req.on('data',(chunk)=>{
+        body.push(chunk)
+        console.log(body)
+        })
+    return req.on('end',()=>{
+        const parsedBody=Buffer.concat(body).toString();
+        console.log(parsedBody)
+        const message=parsedBody.split("=")[1]
+        fs.writeFile('message.txt',message,(err)=>{
+            res.statusCode=302;
+            res.setHeader('Location','/');
+            
+            return res.end();
+        });
+        
+    })
+       
     }
     else if(req.url==='/home'){
         res.write("<html><body><h1>HOME</h1></body></html>")  
@@ -19,8 +46,8 @@ const server=http.createServer((req,res)=>{
         res.write("<html><body><h1>ABOUT</h1></body></html>") 
         return res.end();  
     }
-    res.write("<html><body><h1>hey,welcome</h1></body></html>")
-    res.end();
+    // res.write("<html><body><h1>hey,welcome</h1></body></html>")
+    // res.end();
    
    
 })
