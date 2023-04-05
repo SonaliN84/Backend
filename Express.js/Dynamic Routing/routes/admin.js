97c600fc3532d9d8b99119368c16b87ef778a6bb
SonaliN84 <sonali.nilekar@gmail.com>
2023-04-05 11:15:15 +0530

    working with query params and cart model created

--- Express.js/Dynamic Routing/routes/admin.js
+++ Express.js/Dynamic Routing/routes/admin.js
@@ -12,7 +12,18 @@ router.get('/add-product', adminController.getAddProduct);
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
+router.get('/edit-product/:productId',adminController.getEditProduct)
+
+// router.get('/delete-product/:productId',adminController.getDeleteProduct)
+
+router.post('/delete-product',adminController.postDeleteProduct)
+
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+router.post('/edit-product',adminController.postEditProduct)
+
+
+
+               
 module.exports = router;
