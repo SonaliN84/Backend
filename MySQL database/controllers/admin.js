e66d9e9df813f51e6cec63a91006425b7cc2a6c6
SonaliN84 <sonali.nilekar@gmail.com>
2023-04-06 19:28:37 +0530

    Adding one to many relationship

--- MySQL database/controllers/admin.js
+++ MySQL database/controllers/admin.js
@@ -16,13 +16,22 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  //to add new product in a table and also that product should have association with user...i.e. it should has userId of ther user it belongd to..we use createProduct() method where "Product" in createProduct refers to Product model
+  req.user.createProduct({
+    title:title,
+    price:price,
+    imageUrl:imageUrl,
+    description:description,
+  })
+  /////OR/////
   //to add new product to table use create() in sequelize
- Product.create({
-  title:title,
-  price:price,
-  imageUrl:imageUrl,
-  description:description
- })
+//  Product.create({
+//   title:title,
+//   price:price,
+//   imageUrl:imageUrl,
+//   description:description,
+//   // userId:req.user.id
+//  })
  .then((resullt)=>{
   console.log(resullt)
   res.redirect('/admin/products')
@@ -39,7 +48,11 @@ exports.getEditProduct = (req, res, next) => {
   }
 
   const prodId=req.params.productId;
-  Product.findByPk(prodId).then(product=>{
+ //here we are finding product by id but only products that are associated with that user
+
+  req.user.getProducts({where:{id:prodId}})//getProducts() will give array
+  .then(products=>{
+    const product=products[0];
     if(!product){
       return res.redirect('/')
     }
@@ -51,6 +64,19 @@ exports.getEditProduct = (req, res, next) => {
       product:product
     });
   })
+  // /////OR///here we are finding product by id in all products list//this also works
+  // Product.findByPk(prodId).then(product=>{ 
+  //   if(!product){
+  //     return res.redirect('/')
+  //   }
+  //   res.render('admin/edit-product', {
+  //     pageTitle: 'Add Product',
+  //     path: '/admin/edit-product',
+      
+  //     editing:editMode,
+  //     product:product
+  //   });
+  // })
   .catch(err=>{
     console.log(err)
    })
@@ -87,7 +113,17 @@ exports.postEditProduct=(req,res,next)=>{
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then((products)=>{
+  // //this will give us all the products in product table but we want products of that user only hence we will use another approach.
+  // Product.findAll().then((products)=>{
+  //   res.render('admin/products', {
+  //     prods: products,
+  //     pageTitle: 'Admin Products',
+  //     path: '/admin/products'
+  //   });
+  // })
+
+  req.user.getProducts()
+  .then((products)=>{
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
